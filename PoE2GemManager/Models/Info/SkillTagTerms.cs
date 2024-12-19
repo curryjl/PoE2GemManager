582e4c079efb60c4939ffbef4d42e00891a2a3ca
James Curry <hkcurry@gmail.com>
2024-12-19 12:06:33 -0600

    add skill tag terms

---
+++ PoE2GemManager/Models/Info/SkillTagTerms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoE2GemManager.Models.Info
+{
+    public class SkillTagTerms
+    {
+        public required string Term { get; set; }
+        public required string Definition { get; set; }
+    }
+}
